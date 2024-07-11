141335b9f8fd09868a56d2e1f202bb940a40d3e5
Shashank BN <shashankbn2023@gmail.com>
2024-07-11 13:13:04 +0530

    submit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AllProductsPage from './pages/AllProductsPage';
-import ProductDetailsPage from './pages/ProductDetailsPage';
-import { Container } from '@mui/material';
 
-const App = () => {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <Router>
-      <Container>
-        <Routes>
-          <Route path="/" element={<AllProductsPage />} />
-          <Route path="/product/:id" element={<ProductDetailsPage />} />
-        </Routes>
-      </Container>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
